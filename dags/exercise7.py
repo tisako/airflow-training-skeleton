95dbb61c87cfdf151dc87b40fcf7370f145f35c3
Timo Kooreman <tkooreman@bol.com>
2019-12-09 15:34:41 +0100

    fix weekday

---
+++ dags/exercise7.py
@@ -0,0 +1,26 @@
+from datetime import timedelta
+
+# noinspection PyPackageRequirements
+import airflow
+# noinspection PyPackageRequirements
+from airflow import DAG
+from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
+
+args = {
+    'owner': 'Airflow',
+    'start_date': airflow.utils.dates.days_ago(9),
+}
+
+
+with DAG(
+    dag_id='exercise6',
+        default_args=args,
+        schedule_interval=timedelta(hours=2.5)
+) as dag:
+    PostgresToGoogleCloudStorageOperator(
+        postgres_conn_id='gddconnection',
+        google_cloud_storage_conn_id="google_cloud_storage_default",
+        sql="select * from land_registry_price_paid_uk where transfer_date == {{ execution_date.strftime('%Y-%m-%d') }}",
+        bucket='nice_bucket',
+        filename='{{execution_date}}'
+    )
