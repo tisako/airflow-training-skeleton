ab9d80ece42cd59783c3a2d425a420c8f4d18455
Timo Kooreman <tkooreman@bol.com>
2019-12-10 11:04:43 +0100

    fix httphook

--- dags/hooks/rocket_hook.py
+++ dags/hooks/rocket_hook.py
@@ -2,5 +2,12 @@ from airflow.hooks.http_hook import HttpHook
 
 
 class RocketHook(HttpHook):
+    def __init__(
+            self,
+            method='POST',
+            http_conn_id='rocket_connection'
+    ):
+        super().__init__(method, http_conn_id)
+
     def get_lauches(self, startdate, enddate):
-        return HttpHook.run(self, f"https://launchlibrary.net/1.4/launch?startdate={startdate}&enddate={enddate}")
+        return super().run(endpoint=f"https://launchlibrary.net/1.4/launch?startdate={startdate}&enddate={enddate}")
